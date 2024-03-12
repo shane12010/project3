8d8db91e603b3dff4eb75bdbab0c413581c38722
shane12010 <9016601@student.zadkine.nl>
2024-03-12 11:49:00 +0100

    orientation

---
+++ add_user.php
@@ -0,0 +1,20 @@
+<?php include 'project.php';
+
+$sql = "INSERT INTO users (username, password, email)
+VALUES ('John', 'Doe', 'john@ex')";
+ 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+ 
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+ 
+$conn->close();
+?>
---
+++ inserformulier.php
@@ -0,0 +1,38 @@
+
+<?php
+try {
+    $db = new PDO("mysql:host=localhost;dbname=project3x", "root", "");
+
+    if (isset($_POST['verzenden'])) {
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
+        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_STRING);
+
+
+        $query = $db->prepare("INSERT INTO users(username, password,email) VALUES (:username, :password,:email)");
+        $query->bindParam(":username", $username);
+        $query->bindParam(":password", $password); 
+        $query->bindParam(":email", $email);
+
+        if ($query->execute()) {
+            echo "The new user has been added.";
+        } else {
+            echo "An error occurred!";
+        }
+        echo "<br>";
+    }
+} catch (PDOException $e) {
+    die("Error!: " . $e->getMessage());
+}
+?>
+<form method="POST" action="/project3x/inserformulier.php">
+    <label for="username">Username:</label>
+    <input type="text" name="username" id="username" required><br>
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password" required><br>
+    <label for="email">email:</label>
+    <input type="email" name="email" id="email" required><br>
+
+    <input type="submit" name="verzenden" value="Submit">
+</form>
