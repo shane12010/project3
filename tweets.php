4ddfaaabeec061f49d49b1ab992af5de5a08c959
shane12010 <9016601@student.zadkine.nl>
2024-03-25 13:30:27 +0100

    fix

--- tweets.php
+++ tweets.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 b<?php $title = 'tweets' ?>
 <?php require 'components/header.php' ?>
 <?php require 'components/conn ?>
@@ -11,17 +11,4 @@ foreach ($tweets as $tweet){
  echo $tweet['content'] . </br>
 
 >?
-=======
-b<?php $title = 'tweets' ?>
-<?php require 'components/header.php' ?>
-<?php require 'components/conn' ?>
 
-$pak_alle_tweet = $conn->prepare(query:"SELECT * FROM tweets");
-$pak_alle_tweets->execute();
-$tweets = $pak_alle_tweets->fetchALL();
-
-foreach ($tweets as $tweet){
- echo $tweet['content'] . </br>
-
->?
->>>>>>> e5f6f59cb6ecca7ed325348b5b518c7f46a246ab
