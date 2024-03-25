c7dc0b1b8bc2c3e89a639048449e04ef27eff3f8
shane12010 <9016601@student.zadkine.nl>
2024-03-25 13:24:36 +0100

    tweets

--- tweets.php
+++ tweets.php
@@ -1,6 +1,6 @@
 b<?php $title = 'tweets' ?>
 <?php require 'components/header.php' ?>
-<?php require 'components/conn ?>
+<?php require 'components/conn' ?>
 
 $pak_alle_tweet = $conn->prepare(query:"SELECT * FROM tweets");
 $pak_alle_tweets->execute();
@@ -9,5 +9,4 @@ $tweets = $pak_alle_tweets->fetchALL();
 foreach ($tweets as $tweet){
  echo $tweet['content'] . </br>
 
- 
 >?
